c762ce192b56b2d92d4ff8a05f120148a49897c6
hoantran <hoantran@gmail.com>
2022-12-12 03:02:46 -0500

    Initial Commit

---
+++ GymMembership/GymMembershipApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymMembershipApp.swift
+//  GymMembership
+//
+//  Created by Hoan Tran on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GymMembershipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
